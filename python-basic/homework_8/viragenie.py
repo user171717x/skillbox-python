ea1813c5f1c9ea70268929fa35d06df1ef00a02f
user171717x <user171717user@protonmail.com>
2022-02-07 13:36:04 +0200

    070222

--- python-basic/homework_8/viragenie.py
+++ python-basic/homework_8/viragenie.py
@@ -3,7 +3,7 @@ res = 1
 try:
     for number in range(1, 7):
         arg = 2 ** number
-        res = res * (x - arg - 1) / (x - arg)
+        res = res * (x - (arg - 1)) / (x - arg)
     print(f"Результат ряда res = {res}")
 except ZeroDivisionError:
     print(f"Недопустимое значение х для этого выражения")
