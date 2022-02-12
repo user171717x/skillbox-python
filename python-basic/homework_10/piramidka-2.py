ddf586beb3c1124cf96e160aa5a3727c860be861
user171717x <user171717user@protonmail.com>
2022-02-12 11:46:20 +0200

    update

--- python-basic/homework_10/piramidka-2.py
+++ python-basic/homework_10/piramidka-2.py
@@ -1,8 +1,7 @@
 height = int(input('Введите высоту треугольника: '))
 current = 1
 for row in range(0, height + 1):
-    for _ in range(0, height - row):
-        print('\t', end='')
+    print('\t' * (height - row), end='')
     for _ in range(0, row):
         print(f"{current}\t\t", end='')
         current += 2
--- python-basic/homework_10/yama.py
+++ python-basic/homework_10/yama.py
@@ -11,3 +11,13 @@ for row in range(1, number + 1):
         else:
             print('.', end='')
     print()
+
+# depth = int(input('Введите число для образования ямы: '))
+# for line in range(depth):
+#     for left_number in range(depth, depth - line - 1, -1):
+#         print(left_number, end='')
+#     point_count = 2 * (depth - line -1)
+#     print('.' * point_count, end='')
+#     for right_number in range(depth - line, depth + 1):
+#         print(right_number, end='')
+#     print()
